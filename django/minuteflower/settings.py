8f86c5386770b537e1c292ab7e6be8dd510dcf6d
Charu <charu.gangal@vvdntech.com>
2014-03-11 17:28:27 +0530

    changing settings for sendign out email

--- django/minuteflower/settings.py
+++ django/minuteflower/settings.py
@@ -6,7 +6,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'charu.gangal@vvdntech.com'
-EMAIL_HOST_PASSWORD = 'liferock$1'
+EMAIL_HOST_PASSWORD = '######'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
